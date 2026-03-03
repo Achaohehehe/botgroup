fbf95dfe5c513ad7fe902df6e29b5956318176fc
Achaohehehe <achaohehehe@gmail.com>
2026-03-03 16:28:46 +0800

    Update groups.ts

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -11,7 +11,7 @@ export const groups: Group[] = [
   id: "hongdougao",                
   name: "红豆糕",            
   description: "我叫红豆糕，是沉舟动漫社专用的AI模型！欢迎亲友们友好讨论！",  
-  members: ["comic_test"],         
+  members: ["红豆糕"],         
   isGroupDiscussionMode: true     
 },
     id: 'group1',
